dfee455cd6022efa3db283f221e268d109524ef1
Hibi-eth <yenaehibi@gmail.com>
2022-08-10 12:47:19 -0700

    Base pinter

--- print_bases.c
+++ print_bases.c
@@ -1,86 +1,167 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_hex - prints a number in hexadecimal base,
  * in lowercase
  * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
  * Return: the number of char printed
  */
 
 
-int print_hex(va_list l, flags_t *f)
+int print_hex(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 16, 1);
-	int count = 0;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
 
-	if (f->hash == 1 && str[0] != '0')
-		count += _puts("0x");
-	count += _puts(str);
-	return (count);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'x');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
 }
 
 /**
  * print_hex_big - prints a number in hexadecimal base,
  * in uppercase
  * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
- * Return: the number of char printed/
+ * Return: the number of char printed
  */
-int print_hex_big(va_list l, flags_t *f)
+int print_hex_big(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 16, 0);
-	int count = 0;
+	int len = base_len(num, 16);
+	int rem_num;
+	char *hex_rep;
 
-	if (f->hash == 1 && str[0] != '0')
-		count += _puts("0X");
-	count += _puts(str);
-	return (count);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'X');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
 
 }
 /**
  * print_binary - prints a number in base 2
  * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
- * Return: the number of char printed
+ * Return: The length of the number printed
  */
-int print_binary(va_list l, flags_t *f)
+int print_binary(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 2, 0);
+	char *str;
+	int i, len;
+	char *rev_str;
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 2);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (-1);
 
-	(void)f;
-	return (_puts(str));
+	for (i = 0; num > 0; i++)
+	{
+		if (num % 2 == 0)
+			str[i] = '0';
+		else
+			str[i] = '1';
+		num = num / 2;
+	}
+	str[i] = '\0';
+	rev_str = rev_string(str)
+		if (rev_str == NULL)
+			return (-1);
+	write_base(rev_str);
+	free(str);
+	free(rev_str);
+	return (len);
 }
 
 /**
  * print_octal - prints a number in base 8
  * @l: va_list arguments from _printf
- * @f: pointer to the struct that determines
- * if a flag is passed to _printf
- * Description: the function calls convert() which in turns converts the input
- * number into the correct base and returns it as a string
- * Return: the number of char printed
+ * Return: Number of symbols printed to stdout
  */
-int print_octal(va_list l, flags_t *f)
+int print_octal(va_list l)
 {
 	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 8, 0);
-	int count = 0;
+	int len;
+	char *octal_rep;
+	char *rev_str;
 
-	if (f->hash == 1 && str[0] != '0')
-		count += _putchar('0');
-	count += _puts(str);
-	return (count);
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 8);
+
+	octal_rep = malloc(sizeof(char) * len + 1);
+	if (octal_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		octal_rep[len] = (num % 8) + 48;
+		num = num / 8;
+
+	}
+	octal_rep[len] = '\0';
+	rev_str = rev_string(octal_rep);
+	if (rev_str == NULL)
+		return (-1);
+
+	write_base(rev_str);
+	free(octal_rep);
+	free(rev_str);
+	return (len);
 }
+
