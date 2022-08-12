57e06095e306cf638512682f49147faa3549bb76
Hibi-eth <yenaehibi@gmail.com>
2022-08-12 13:10:53 -0700

    header

--- _printf.c
+++ _printf.c
@@ -1,38 +1,31 @@
+
+
+#include <stdarg.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
-
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-	conver_t f_list[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
-	};
-	va_list arg_list;
+	int amount = 0, i;
 
-	if (format == NULL)
-		return (-1);
 
-	va_start(arg_list, format);
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+	va_list data;
+
+
+	va_start(data, format);
+
+
+	for (i = 0; format[i] != '%' && format[i] != '\0'; i++)
+	{
+		amount = amount + _putchar(format[i]);
+	}
+
+
+
+	return (amount);
 }
--- main.h
+++ main.h
@@ -1,56 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-
-
-/**
- * struct convert - defines a structure for symbols and functions
- * on the format specifier passed to printf()
- * @sym: The operator
- * @f: pointer to the correct printing function
- */
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
-
-int print_int(va_list);
-int print_number(va_list);
-int print_unsigned(va_list);
-int count_digit(int i);
-
-int print_hex(va_list);
-int print_hex_big(va_list);
-int print_binary(va_list);
-int print_octal(va_list);
-
-
 int _printf(const char *format, ...);
-
-
-int print_string(va_list);
-int print_char(va_list);
-
 int _putchar(char c);
-int _puts(char *str);
-
-int print_rot13(va_list);
-int print_rev(va_list arg);
-
-
-int print_percent(va_list);
-int parser(const char *format, conver_t f_list[], va_list arg_list);
-int _write_char(char c);
-
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
 
 #endif
